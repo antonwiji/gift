a1a3408731a99bb99831dcfd32e2c18ffb286e58
Anton Wijaya <anton.wijaya@prosia.co.id>
2023-05-06 10:04:01 +0700

    final web

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const onClickButton = () => {
 };
 
 document.getElementById("btnOpen").addEventListener("click", onClickButton);
+
+const getValueMessage = document.getElementById("messageWa")
+
+document.getElementById("btnWa").addEventListener("click", () => {
+  const urlWa = "https://api.whatsapp.com/send?phone=6285709005738&text="
+  window.location.href = urlWa + getValueMessage.value
+})
+
+
